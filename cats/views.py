c9116bb6e87f8996b1161fb96cc686724f28204b
Dmitry <guryaidemon@gmail.com>
2022-04-06 14:01:10 +0300

    Добавил собственные тротлиг класс для ограничения запросов на 2 часа

--- cats/views.py
+++ cats/views.py
@@ -20,7 +20,6 @@ class CatViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'retrieve':
             return (ReadOnly(),)
-
         return super().get_permissions()
 
 
